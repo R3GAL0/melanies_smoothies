9dfdcfc0e93562738dbf769d4d31f0307ff7c5de
mwalent <137714605+R3GAL0@users.noreply.github.com>
2025-11-17 19:36:25 -0500

    Update streamlit_app.py
    add json format for api

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -45,6 +42,10 @@ if ingredients_list:
     st.write(my_insert_stmt)
     # st.stop()
     
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response.json())
+
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
