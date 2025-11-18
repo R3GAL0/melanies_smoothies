04f8625a31633f84fd89e8a4d6cadcc29edee89f
mwalent <137714605+R3GAL0@users.noreply.github.com>
2025-11-17 19:38:01 -0500

    Update streamlit_app.py
    added df instead of json

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,17 +34,18 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+    # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
     # st.stop()
     
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+    # st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_reponse.json(), use_container_width=Truew
 
     time_to_insert = st.button('Submit Order')
 
